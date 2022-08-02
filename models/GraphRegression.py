b12940b9446bad0a28fe4572fa32ff8106b3e44d
anoncodesub <106044647+anoncodesub@users.noreply.github.com>
2022-08-02 13:54:12 -0500

    update weight initialization
    update weight initialization

--- models/GraphRegression.py
+++ models/GraphRegression.py
@@ -35,13 +35,11 @@ class GraphRegression(nn.Module):
         self.regressor = nn.Linear(hidden_size, 1)
 
         self.reset_parameters()
-    def weights_init(self,m):
-        if isinstance(m, nn.Linear):
-            nn.init.xavier_uniform_(m.weight.data)
-            nn.init.zeros_(m.bias.data)
+
     def reset_parameters(self):
         self.embedding_model.reset_parameters()
-        self.regressor.apply(self.weights_init)
+        nn.init.xavier_normal_(self.regressor.weight.data)
+        nn.init.zeros_(self.regressor.weight.data)
         if self.pooling_method == "attention":
             self.pool.reset_parameters()
 
